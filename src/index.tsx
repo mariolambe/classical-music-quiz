721f08bbabde264098e7983fb5cc46173e2ccac9
mariolambe <61537859+mariolambe@users.noreply.github.com>
2024-09-19 21:10:06 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './styles.css';
 
-const container = document.getElementById("root");
-const root = createRoot(container!);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
